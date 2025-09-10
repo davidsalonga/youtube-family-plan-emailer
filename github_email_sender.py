778bae6a90bf9277998f6831f6aab6aff1f308f2
david.salonga <salongadaviid@gmail.com>
2025-09-10 17:58:51 +0800

    trigger prd test

--- github_email_sender.py
+++ github_email_sender.py
@@ -27,8 +27,6 @@ class GitHubEmailSender:
         
         # YouTube Family Plan recipients
         self.recipients = [
-            'sophiaypa@gmail.com',          # Sophia Aguilar
-            'jeff.rosarito@gmail.com',      # Jeffrey Rosarito
             'azorlanac@gmail.com',          # Azor Lanac
             'salongadaviid@gmail.com'       # David Salonga
         ]
